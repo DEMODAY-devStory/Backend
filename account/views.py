021c9b667cf5084332480fd45a000632fb1fed6e
sen <seeunchoi99@gmail.com>
2022-10-03 23:45:29 +0900

    ✨로그인, 로그아웃 view 추가

--- account/views.py
+++ account/views.py
@@ -1,17 +1,19 @@
+from django.contrib.auth import login, logout
 from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, GenericAPIView
 from rest_framework.permissions import AllowAny
 
-from .serializers import UserSerializer
+from .serializers import *
 
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
 
 @method_decorator(csrf_exempt, name='dispatch')
-class UserView(CreateAPIView):
+class UserSignUpView(CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
 
@@ -24,37 +26,30 @@ class UserView(CreateAPIView):
             # 데이터가 unique하지 않음
             return Response(data={'message': "이미 가입된 회원입니다"}, status=status.HTTP_400_BAD_REQUEST)
         except Exception:
-            # 기타 오류 (발생하지 않을 것으로 추정)
+            # 기타 오류 (발생하면 안 됨)
             return Response(exception=Exception)
 
         serializer.save()
         return Response(status=status.HTTP_201_CREATED)
 
-# @method_decorator(csrf_exempt, name='dispatch')
-# class UserLoginView(GenericAPIView):
-#     permission_classes = (AllowAny,)
-#     serializer_class = UserLoginSerializer
+@method_decorator(csrf_exempt, name='dispatch')
+class UserLoginView(GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserLoginSerializer
 
-#     def post(self, request):
-#         serializer = self.serializer_class(data=request.data)
-#         user = serializer.validate(request.data)
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        user = serializer.validate(request.data)
 
-#         if user is None:
-#             response = {
-#                 "success": "false",
-#                 "status_code": status.HTTP_200_OK,
-#                 "id": "",
-#             }
-#         else:
-#             try:
-#                 img = UserImg.objects.get(user_id=user.id).image
-#             except:
-#                 img = ""
-#             response = {
-#                 "success": "true",
-#                 "status_code": status.HTTP_200_OK,
-#                 "id": user.id,
-#                 "img": str(img),
-#             }
+        if user is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            user.before_last_login = user.last_login
+            login(request, user)
+            return Response(data={'id': user.id}, status=status.HTTP_200_OK)
 
-#         return Response(response, status=status.HTTP_200_OK)
+@method_decorator(csrf_exempt, name='dispatch')
+@api_view(('GET',))
+def UserLogoutView(request):
+    logout(request)
+    return Response(status=status.HTTP_200_OK)
