57703715dd356672d8207b354d9ac1bf3d523421
sen <seeunchoi99@gmail.com>
2022-10-03 17:04:30 +0900

    🔥UserSerializer.create() 삭제

--- account/serializers.py
+++ account/serializers.py
@@ -9,7 +9,3 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('email', 'password', 'nickname', 'name')
         extra_kwargs = {"password": {"write_only": True}}
 
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-
-        return user
