6df2cafc23a652cbb47399934618a94f3e995bed
sen <seeunchoi99@gmail.com>
2022-10-03 23:44:18 +0900

    🔥유저 모델 코드 정리 및 언어 영어로 수정

--- account/models.py
+++ account/models.py
@@ -1,56 +1,37 @@
+from django.contrib.auth.hashers import make_password
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
-import uuid
-from django.conf import settings
 from django.utils import timezone
-
+import uuid
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, name, email, nickname, password=None):
-        if not email:
-            raise ValueError("Users Must Have an email address")
-        user = self.model(
-            name=name,
-            email=self.normalize_email(email),
-            nickname=nickname,
-        )
-        user.set_password(password)  # 비밀번호를 hash로 저장 (보안)
-
+    def _create_user(self, email, password, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, nickname, email, name, password):
-        if password is None:
-            raise TypeError("Superusers must have a password.")
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
 
-        user = self.create_user(nickname, email, name, password)
-        user.is_superuser = True
-        user.is_staff = True
-        user.is_active = True
-        user.save()
-
-        return user
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.EmailField(
-        verbose_name='email address',
-        max_length=255,
-        unique=True
-    )
-    is_active = models.BooleanField(default=True)
+    email = models.EmailField(max_length=50, unique=True)
     is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
     before_last_login = models.DateTimeField(default=timezone.now)
-    nickname = models.CharField(
-        max_length=20,
-        unique=True,
-    )
-    name = models.CharField(max_length=10)
+    nickname = models.CharField(max_length=15)
+    name = models.CharField(max_length=15)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['nickname', 'name']
