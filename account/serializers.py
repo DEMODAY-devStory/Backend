097a8173eb9ed12070964a2d697ebc63df4e0fc7
sen <seeunchoi99@gmail.com>
2022-10-03 23:45:12 +0900

    🔥✨회원가입 시 create_user() 실행하게 수정, 로그인 시리얼라이저 추가

--- account/serializers.py
+++ account/serializers.py
@@ -9,3 +9,19 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('email', 'password', 'nickname', 'name')
         extra_kwargs = {"password": {"write_only": True}}
 
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+    # TODO: 회원정보 수정 시 update 함수 override
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        email = data.get("email", None)
+        password = data.get("password", None)
+        user = authenticate(email=email, password=password)
+
+        return user
