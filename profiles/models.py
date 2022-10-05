909cec884f72a0f86316b451f32fb1dedaa3dd2e
sen <seeunchoi99@gmail.com>
2022-10-06 01:38:45 +0900

    ✨프로필 모델에 출력용 양식 추가

--- profiles/models.py
+++ profiles/models.py
@@ -13,6 +13,9 @@ class Profile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return str(self.user)
+
 
 class Study(models.Model):
     profile = models.OneToOneField('Profile', on_delete=models.CASCADE)
@@ -20,12 +23,18 @@ class Study(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return str(self.current_study) + " " + str(self.profile)
+
 
 class Hashtag(models.Model):
     hashtag_name = models.CharField(max_length=20, primary_key=True)
     profile = models.ManyToManyField('Profile')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.hashtag_name)
 
 
 class Skill(models.Model):
@@ -38,6 +47,9 @@ class Skill(models.Model):
     skill_type = models.CharField(max_length=20, choices=CHOICE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.skill_name) + " " + str(self.profile)
 
 
 class Project(models.Model):
@@ -52,9 +64,12 @@ class Project(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return str(self.project_name) + " " + str(self.profile)
+
 
 class Career(models.Model):
-    user = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
     company = models.CharField(max_length=MAX_LENGTH, null=True)
     position = models.CharField(max_length=MAX_LENGTH, null=True)
     locate = models.CharField(max_length=MAX_LENGTH, null=True)
@@ -65,9 +80,15 @@ class Career(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return str(self.company) + " " + str(self.profile)
+
 
 class Follow(models.Model):
     follower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='follower')
     following = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.follower) + " follows " + str(self.following)
