b2a2097b75625de73303ee6623e2a252ef5ad12e
sen <seeunchoi99@gmail.com>
2022-10-07 14:20:03 +0900

    ✨프로필 view 추가

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from .serializers import *
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+
+class ProfileView(ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    lookup_field = "user"
+
+
+class StudyView(ModelViewSet):
+    queryset = Study.objects.all()
+    serializer_class = StudySerializer
+    lookup_field = "profile"
+
+
+class HashtagView(ModelViewSet):
+    queryset = Hashtag.objects.all()
+    serializer_class = HashtagSerializer
+    lookup_field = "profile"
+
+
+class SkillView(ModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+    lookup_field = "profile"
+
+
+class ProjectView(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProfileSerializer
+    lookup_field = "profile"
+
+
+class CareerView(ModelViewSet):
+    queryset = Career.objects.all()
+    serializer_class = CareerSerializer
+    lookup_field = "profile"
+
+
+class FollowView(ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
