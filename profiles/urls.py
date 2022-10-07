92f860e707c589aaaea3657c8ed779cd3f26d909
sen <seeunchoi99@gmail.com>
2022-10-07 14:19:43 +0900

    ✨프로필 objects, url 추가

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,11 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('profile', ProfileView)
+router.register('study', StudyView)
+router.register('hashtag', HashtagView)
+router.register('skill', SkillView)
+router.register('project', ProjectView)
+router.register('career', CareerView)
+router.register('follow', FollowView)
 
 urlpatterns = router.get_urls()
