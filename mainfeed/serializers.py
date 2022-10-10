11b8ed5810e253ee88f7342d6a012dd1013777c9
taeho0888 <taeho0888@naver.com>
2022-10-10 22:37:09 +0900

    🔥불필요한 import 제거

--- mainfeed/serializers.py
+++ mainfeed/serializers.py
@@ -1,6 +1,4 @@
 from rest_framework import serializers
-from account.models import User
-from profiles.models import Follow
 from profiles.models import Profile
 
 class FriendsSerializer(serializers.ModelSerializer):
