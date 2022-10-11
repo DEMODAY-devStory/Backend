61887f496a1f1bd5165a58f2a255e37dd0470c84
sen <seeunchoi99@gmail.com>
2022-10-11 23:24:12 +0900

    🔥recommendView 코드 정리

--- mainfeed/views.py
+++ mainfeed/views.py
@@ -6,13 +6,15 @@ from account.models import User
 
 from .serializers import *
 
+
 class RecommendView(ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = RecommendSerializer
 
     def retrieve(self, request, *args, **kwargs):
-        queryset = Profile.objects.exclude(user=self.request.user).filter(Hashtag=kwargs['pk']) # 자기자신 제외 해시태그 갖고있는 사람들
-        following_list = User.objects.get(id=self.request.user).follower.values_list('following_id', flat=True) # request user가 팔로우하고 있는 사람 리스트
-        for i in following_list: queryset = queryset.exclude(user=i) # 리스트에서 한 명 씩 제거
+        queryset = Profile.objects.exclude(user=self.request.user).filter(Hashtag=kwargs['pk'])
+        following_list = User.objects.get(id=self.request.user).follower.values_list('following_id', flat=True)
+        for following in following_list:
+            queryset = queryset.exclude(user=following)
         serializer = self.get_serializer(queryset[:5], many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
