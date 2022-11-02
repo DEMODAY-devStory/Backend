f56c22b8e2f62219dfb39a1510ff268b9e5a15fb
taeho0888 <taeho0888@naver.com>
2022-11-02 14:20:36 +0900

    🔥주석 제거

--- mainfeed/views.py
+++ mainfeed/views.py
@@ -1,7 +1,6 @@
 import operator
 from rest_framework import status
 from rest_framework.response import Response
-# from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListAPIView
 
 from account.models import User
@@ -40,12 +39,3 @@ class RecommendView(ListAPIView):
 
         serializer = self.get_serializer(queryset[:5], many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-'''
-    def retrieve(self, request, *args, **kwargs):
-        queryset = Profile.objects.exclude(user=self.request.user).filter(Hashtag=kwargs['pk']) # 자기자신 제외 해시태그 갖고있는 사람들
-        following_list = User.objects.get(id=self.request.user).follower.values_list('following_id', flat=True) # request user가 팔로우하고 있는 사람 리스트
-        for i in following_list: queryset = queryset.exclude(user=i) # 리스트에서 한 명 씩 제거
-        serializer = self.get_serializer(queryset[:5], many=True)
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
-'''
