b5b0dda109fbc014059e4a7ba468cc182fb1bd40
sen <seeunchoi99@gmail.com>
2022-11-02 15:23:19 +0900

    🔥mainfeed.views import 정렬

--- mainfeed/views.py
+++ mainfeed/views.py
@@ -1,6 +1,5 @@
 from rest_framework import status, generics
 import operator
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView
 
