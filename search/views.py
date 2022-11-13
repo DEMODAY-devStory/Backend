428f1b328282f902f479972840f910a71da78347
sen <seeunchoi99@gmail.com>
2022-11-13 17:43:13 +0900

    ✨검색 기능 구현
    이름, 해시태그로 검색 가능

--- search/views.py
+++ search/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from profiles.models import *
+from account.models import *
+
+
+class SearchView(ListAPIView):
+    def get_queryset(self):
+        return None
+
+    def list(self, request, *args, **kwargs):
+        search_string = kwargs['string']
+        searched_profiles = set()
+
+        hashtags = Hashtag.objects.filter(hashtag_name__icontains=search_string)
+        for hashtag in hashtags:
+            profiles_queryset = hashtag.profile.all().values_list('user_id', flat=True)
+            searched_profiles.update(profiles_queryset)
+
+        users = User.objects.exclude(id=self.request.user.id) \
+            .filter(id__icontains=search_string).values_list('id', flat=True)
+        searched_profiles.update(users)
+
+        return Response(data={"ids": searched_profiles}, status=status.HTTP_200_OK)
