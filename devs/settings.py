c2b1ad3fdc1ddd574e80bb56ea6e3e7d1a382870
sen <seeunchoi99@gmail.com>
2022-11-16 22:17:36 +0900

    🚀배포를 위한 설정 수정

--- devs/settings.py
+++ devs/settings.py
@@ -44,7 +44,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.sogangfestival.com']
 
 REST_USE_JWT = True
 
