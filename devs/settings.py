bb6c8146c2f667b7c9e6ca5b138d6fd145fa9126
sen <seeunchoi99@gmail.com>
2022-11-18 02:15:06 +0900

    🚀CORS_ALLOWED_ORIGINS 수정

--- devs/settings.py
+++ devs/settings.py
@@ -101,7 +101,7 @@ CORS_ORIGIN_WHITELIST = [
 CORS_ALLOWED_ORIGINS = [
     'https://www.sogangfestival.com',
     'https://main.d359crvpmuv173.amplifyapp.com',
-    'devs-tory.herokuapp.com'
+    'https://devs-tory.herokuapp.com'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
